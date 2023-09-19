c5ab883ce1ba92264b7b8104be3bd05f3d807485
CC <my-heart-now.dazzler118@passinbox.com>
2023-09-19 17:38:09 +0300

    add switcher

---
+++ Translate/rc/__Git_switch.cmd
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set MIDIMT_LANG_DIR=S:\__BuildSource\__SVN__\EasyControl-Mackie\EasyControl-Mackie-src\src\Languages
+git checkout localsrc
+git merge main
+git commit -a -m "auto update"
+
+:END
+endlocal
+exit 0
